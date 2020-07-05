19b945190159dde864f4e6d95a977f759c82eb6a
brolisdom <brolisdom99@gmail.com>
2020-07-04 20:00:57 -0500

    commit on 'master'

--- src/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const MONGODB_URI = 'mongodb://localhost/pruebas'; //cambiar
-
-mongoose.connect(MONGODB_URI, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useCreateIndex: true
-}).then(db => console.log('Database is connected')).catch(err => console.log(err));
